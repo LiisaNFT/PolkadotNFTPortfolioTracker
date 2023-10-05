f2a150345f7ba09c48631e02f0d2042a83d82be4
Paulo <paulogustavopeixoto@gmail.com>
2023-10-05 19:25:04 +0100

    update functions

--- Scripts/dataQuery.js
+++ Scripts/dataQuery.js
@@ -1,70 +1,14 @@
-const axios = require('axios');
+import { fetchcontractFunctionRuns, fetchcontractEventTradeAccepteds } from '../functions/getSales/getSalesAstar.js';
 
-// Protocol graphql urls
+// Variables for queiring graphql
 
-const Moonbeam_Moonbeans = "http://localhost:4350/graphql";
+const hostAstarTofuNFT = "http://localhost:4350/graphql";
+const hostMoonbeamMoonbeans = "http://localhost:4350/graphql";
+const blockNumber_gte = 45672;
+const limit = 10;
 
+//Functions
 
-function fetchcontractFunctionRuns() {
-    const query = `
-    {
-      contractFunctionRuns(orderBy: blockNumber_DESC, limit: 10, where: {blockNumber_gte: 4567266}) {
-        blockNumber
-        blockTimestamp
-        contract
-        detail
-        functionName
-        functionSuccess
-        functionValue
-        id
-        intent
-        sigDetail
-        sigIntent
-        transactionHash
-      }
-    }
-    `;
-    // Make the request to receive data from 
-    axios.post(Moonbeam_Moonbeans, { query: query })
-        .then(response => {
-            console.log(JSON.stringify(response.data, null, 4));
-        })
-        .catch(error => {
-            console.error("Error querying GraphQL:", error);
-        });
-}
+fetchcontractFunctionRuns(hostAstarTofuNFT, blockNumber_gte, limit);
 
-function fetchContractEventTradeAccepteds() {
-  const query = `
-  {
-      contractEventTradeAccepteds(where: {timestamp_lt: "1694025660"}) {
-        blockNumber
-        blockTimestamp
-        contract
-        eventName
-        id
-        expiry
-        newOwner
-        oldOwner
-        price
-        quantity
-        timestamp
-        token
-        tradeId
-        tokenId
-        tradeType
-        transactionHash
-      }
-    }
-  `;
-  // Make the request to receive data from 
-  axios.post(Moonbeam_Moonbeans, { query: query })
-      .then(response => {
-          console.log(JSON.stringify(response.data, null, 4));
-      })
-      .catch(error => {
-          console.error("Error querying GraphQL:", error);
-      });
-}
-
-fetchcontractFunctionRuns();
+fetchcontractEventTradeAccepteds(hostMoonbeamMoonbeans, blockNumber_gte, limit);
---
+++ functions/getSales/getSalesAstar.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Main functions
+
+export function fetchcontractFunctionRuns(host, blockNumber_gte, limit) {
+    // Load the GraphQL query from the file
+    console.log('Hello World');
+    const queryFilePath = path.join(__dirname, '../../queries/Astar/contractFunctionRuns.graphql');
+    const query = fs.readFileSync(queryFilePath, 'utf8');
+    
+    const variables = {
+        blockNumber_gte: blockNumber_gte,
+        limit: limit
+    };
+
+    // Make the request to receive data from 
+    axios.post(host, { query: query, variables: variables })
+        .then(response => {
+            console.log(JSON.stringify(response.data, null, 4));
+        })
+        .catch(error => {
+            console.error("Error querying GraphQL:", error);
+        });
+}
+
+export function fetchcontractEventTradeAccepteds(host, blockNumber_gte, limit) {
+    // Load the GraphQL query from the file
+    console.log('Hello World');
+    const queryFilePath = path.join(__dirname, '../../queries/Moonbeam/contractEventTradeAccepteds.graphql');
+    const query = fs.readFileSync(queryFilePath, 'utf8');
+    
+    const variables = {
+        blockNumber_gte: blockNumber_gte,
+        limit: limit
+    };
+
+    // Make the request to receive data from 
+    axios.post(host, { query: query, variables: variables })
+        .then(response => {
+            console.log(JSON.stringify(response.data, null, 4));
+        })
+        .catch(error => {
+            console.error("Error querying GraphQL:", error);
+        });
+}
