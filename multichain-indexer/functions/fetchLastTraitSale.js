d31bfd8709a9ab0015f3b10622cf734453b96564
Paulo <paulogustavopeixoto@gmail.com>
2024-02-19 15:48:43 +0000

    functions/ updated : fetchNftAcquisitionPrice

--- multichain-indexer/functions/fetchLastTraitSale.js
+++ multichain-indexer/functions/fetchLastTraitSale.js
@@ -1,13 +1,9 @@
-import axios from 'axios';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const { request } = require('graphql-request');
+const fs = require('fs');
+const path = require('path');
 
 //Collection - Last Sale per trait
-export function fetchLastTraitSale(host, collectionId, attributeType) {
+export async function fetchLastTraitSale(host, collectionId, attributeType) {
     // Load the GraphQL query from the file
     const queryFilePath = path.join(__dirname, '../src/queries/getLastTraitSale.graphql');
     const query = fs.readFileSync(queryFilePath, 'utf8');
@@ -17,15 +13,15 @@ export function fetchLastTraitSale(host, collectionId, attributeType) {
         attributeType: attributeType
     };
 
-    // Make the request to receive data from 
-    axios.post(host, { query: query, variables: variables })
-        .then(response => {
-            console.log(JSON.stringify(response.data, null, 4));
-        })
-        .catch(error => {
-            console.error("Error querying GraphQL:", error.message);
-            if (error.response && error.response.data && error.response.data.errors) {
-                console.error("GraphQL Errors:", JSON.stringify(error.response.data.errors, null, 2));
-            }
-        });
+    try {
+        const endpoint = `${host}/graphql`;
+        
+        const response = await request(endpoint, query, variables);
+        console.log(JSON.stringify(response, null, 4));
+    } catch (error) {
+        console.error("Error querying GraphQL:", error.message);
+        if (error.response && error.response.errors) {
+            console.error("GraphQL Errors:", JSON.stringify(error.response.errors, null, 2));
+        }
+    }
 }
--- multichain-indexer/functions/fetchNftAcquisitionPrice.js
+++ multichain-indexer/functions/fetchNftAcquisitionPrice.js
@@ -1,13 +1,9 @@
-import axios from 'axios';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const { request } = require('graphql-request');
+const fs = require('fs');
+const path = require('path');
 
 //NFT - Acquisition price
-export function fetchNftAcquisitionPrice(host, nftId) {
+export async function fetchNftAcquisitionPrice(host, nftId) {
     // Load the GraphQL query from the file
     const queryFilePath = path.join(__dirname, '../src/queries/LastNftTransaction.graphql');
     const query = fs.readFileSync(queryFilePath, 'utf8');
@@ -16,15 +12,15 @@ export function fetchNftAcquisitionPrice(host, nftId) {
         nftId: nftId
     };
 
-    // Make the request to receive data from 
-    axios.post(host, { query: query, variables: variables })
-        .then(response => {
-            console.log(JSON.stringify(response.data, null, 4));
-        })
-        .catch(error => {
-            console.error("Error querying GraphQL:", error.message);
-            if (error.response && error.response.data && error.response.data.errors) {
-                console.error("GraphQL Errors:", JSON.stringify(error.response.data.errors, null, 2));
-            }
-        });
+    try {
+        const endpoint = `${host}/graphql`;
+        
+        const response = await request(endpoint, query, variables);
+        console.log(JSON.stringify(response, null, 4));
+    } catch (error) {
+        console.error("Error querying GraphQL:", error.message);
+        if (error.response && error.response.errors) {
+            console.error("GraphQL Errors:", JSON.stringify(error.response.errors, null, 2));
+        }
+    }
 }
